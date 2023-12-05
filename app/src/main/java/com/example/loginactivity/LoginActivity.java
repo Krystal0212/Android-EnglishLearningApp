c10cedbddf32708056fdbb28c9a6ce9e149d1473
Krystal0212 <ntu4318@gmail.com>
2023-12-06 02:49:11 +0700

    06/12/23 2:48 Am Update Login Page and Sign Up Page

--- app/src/main/java/com/example/loginactivity/LoginActivity.java
+++ app/src/main/java/com/example/loginactivity/LoginActivity.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -25,10 +27,9 @@ import com.google.firebase.database.FirebaseDatabase;
 public class LoginActivity extends AppCompatActivity {
 
     TextView signUp;
-
     EditText edtEmail, edtPassword;
-    Button btnSignin;
-
+    Button btnSignIn;
+    ImageButton btnRememberMe;
     ProgressBar progressBar;
 
     @Override
@@ -43,8 +44,9 @@ public class LoginActivity extends AppCompatActivity {
         signUp = findViewById(R.id.txtSignUp);
         edtEmail = findViewById(R.id.edtTxtEmail);
         edtPassword = findViewById(R.id.edtTxtPassword);
-        btnSignin = findViewById(R.id.btnSignIn);
+        btnSignIn = findViewById(R.id.btnSignIn);
         progressBar = findViewById(R.id.progressBar);
+        btnRememberMe = findViewById(R.id.btnRememberMe);
     }
 
     private void initListener() {
@@ -56,10 +58,21 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        btnSignin.setOnClickListener(new View.OnClickListener() {
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onClickSignIn();
+            }
+        });
+
+        btnRememberMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (btnRememberMe.isSelected()) {
+                    btnRememberMe.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ticked));
+                } else {
+                    btnRememberMe.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.unticked));
+                }
             }
         });
     }
