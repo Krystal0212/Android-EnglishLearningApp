a8c46fe11a742bc76ce42921c66650f3cacb67a4
DungChi <tridungluong123@gmail.com>
2023-12-09 05:27:12 +0700

    auto run

--- app/src/main/java/com/example/loginactivity/TopicDetailActivity.java
+++ app/src/main/java/com/example/loginactivity/TopicDetailActivity.java
@@ -22,7 +22,7 @@ public class TopicDetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_topic_detail);
+        setContentView(R.layout.activity_topic);
 
         initUI();
         initListener();
