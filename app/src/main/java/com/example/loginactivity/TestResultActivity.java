826e0c0528b179d9da30c21a4fdbf494977e3217
Krystal0212 <ntu4318@gmail.com>
2023-12-13 19:39:42 +0700

    3:20 PM 13/12/23 Modify Testing layout

---
+++ app/src/main/java/com/example/loginactivity/TestResultActivity.java
@@ -0,0 +1,55 @@
+package com.example.loginactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.loginactivity.models.Question;
+
+import java.util.ArrayList;
+
+public class TestResultActivity extends AppCompatActivity {
+    ArrayList<Question> incorrectlyAnsweredQuestions = new ArrayList<>();
+    ArrayList<Question> questions = new ArrayList<>();
+
+    Button btn_Topic;
+    TextView txtResult, txtPoint;
+    private static int point = 500;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flashcard_complete_testing);
+
+        initUI();
+        initListener();
+    }
+
+    private void initUI() {
+        btn_Topic = findViewById(R.id.btn_back_Topic);
+        txtResult = findViewById(R.id.txtResult);
+        txtPoint = findViewById(R.id.txtSentenceRight);
+
+        Intent intent = getIntent();
+        incorrectlyAnsweredQuestions = intent.getParcelableArrayListExtra("incorrectlyAnsweredQuestions");
+        questions = intent.getParcelableArrayListExtra("questions");
+
+        if (!(incorrectlyAnsweredQuestions.isEmpty() || questions.isEmpty() )) {
+            int rightNumber = questions.size() - incorrectlyAnsweredQuestions.size();
+            txtPoint.setText("Your point is " + rightNumber * point + " !!!");
+            txtResult.setText(rightNumber + "/" + questions.size());
+        }
+    }
+
+    private void initListener() {
+        btn_Topic.setOnClickListener(v -> {
+            onBackPressed();
+            finish();
+        });
+    }
+}
